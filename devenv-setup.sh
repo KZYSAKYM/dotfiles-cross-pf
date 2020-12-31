e515ee5df944800c0c68ef55097d484c5b6cf67a
KZYSAKYM <akiyama.kazuyoshi.64w@gmail.com>
2020-12-31 11:17:24 +0900

    update

--- devenv-setup.sh
+++ devenv-setup.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
 RECIPES="\
-	nvim_dependencies \
-	nvim_config \
-	vim \
-	vim_config \
-	nvim \
-	rust \
-	bat \
-	exa \
-	findfd \
-	procs \
-	ripgrep \
-	rls \
-	zsh \
-	zsh_prezto \
-	zsh_config \
-	curl \
-	ranger \
+  nvim_dependencies \
+  nvim_config \
+  vim \
+  vim_config \
+  nvim \
+  rust \
+  bat \
+  exa \
+  findfd \
+  procs \
+  ripgrep \
+  rls \
+  zsh \
+  zsh_prezto \
+  zsh_config \
+  curl \
+  ranger \
+  npm \
+  bash-language-server \
+  typescript-language-server \
+  vscode-html-languageserver-bin \
+  bash-it \
+  fzf \
+  tmux \
+  tmux_config \
 "
 
 python3 main.py --recipe $RECIPES
